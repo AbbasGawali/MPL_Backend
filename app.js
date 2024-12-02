dda4179ae8f930fb0292ddb71ed6a9673577cc12
Abbas Gawali <abbasgawali111@gmail.com>
2024-12-02 19:21:09 +0530

    cors

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ import cors from "cors"
 const port = process.env.PORT || 7000;
 const app = express();
 
-app.use(cors({ origin: 'http://localhost:5173' }));
+app.use(cors());
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
